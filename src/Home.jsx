466c7e0978088931076ec3599977bcce74796afe
Arun-G-24 <arunkutty93445@gmail.com>
2025-12-26 16:43:20 +0530

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Chat from '../src/components//Chat'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+
+  const navigete=useNavigate()
   return (
+    <>
+    
     <div>
 <h1 className='text-center font-stretch-90% '>Welcome to our page 🤗🤠</h1>
+
     </div>
+      <button onClick={()=>navigete("/chat")} className='btn btn-bg-bule text-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>Start here..</button>
+    
+    </>
   )
 }
 
