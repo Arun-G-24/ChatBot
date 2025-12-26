dac58046fcd6a070ea851653cdd8f8e289684c90
Arun-G-24 <arunkutty93445@gmail.com>
2025-12-26 17:01:12 +0530

    update

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -9,22 +9,26 @@ function InputBox({ text, setText, sendMessage, suggestions, onSuggestionClick }
       </div>
 
     <div className="p-2 border-t flex gap-2">
-      <input
-          value={text}
+  <input
+    value={text}
+    onChange={(e) => setText(e.target.value)}
+    onKeyDown={(e) => {
+      if (e.key === "Enter") {
+        sendMessage();
+      }
+    }}
+    placeholder="Type here..."
+    className="flex-1 border px-3 py-2 rounded"
+  />
 
-         onChange={(e) => setText(e.target.value)}
-          placeholder="Type here..."
-          className="flex-1 border px-3 py-2 rounded"
+  <button
+    onClick={sendMessage}
+    className="bg-blue-500 text-white px-4 rounded cursor-pointer shadow-blue-500/50 shrink-0"
+  >
+    Send
+  </button>
+</div>
 
-
-        />
-        <button
-        onClick={sendMessage}
-          className="bg-blue-500 text-white px-4 rounded cursor-pointer shadow-blue-500/50 shrink-0"
-        >
-          Send
-        </button>
-      </div>
     </>
   );
 }
